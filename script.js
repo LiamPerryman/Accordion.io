c590eced4a99a9f7ab0b0fa62012f62263822fe0
Liam Perryman <129959249+LiamPerryman@users.noreply.github.com>
2023-06-07 19:13:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+"user-strict";
+
+const dropDownFunctionForHeading = function (number) {
+  document
+    .querySelector(`.question-` + number)
+    .addEventListener(`click`, function () {
+      if (
+        document.querySelector(`.answer-` + number).classList.contains(`hidden`)
+      ) {
+        // Opens dropdown //
+        document.querySelector(`.answer-` + number).classList.remove(`hidden`);
+        document
+          .querySelector(`.question-` + number)
+          .classList.add(`weight-color`);
+        document.querySelector(`.arrow-` + number).classList.add(`rotate`);
+      } else {
+        // Closes dropdown //
+        document.querySelector(`.answer-` + number).classList.add(`hidden`);
+        document
+          .querySelector(`.question-` + number)
+          .classList.remove(`weight-color`);
+        document.querySelector(`.arrow-` + number).classList.remove(`rotate`);
+      }
+    });
+};
+
+dropDownFunctionForHeading(`one`);
+dropDownFunctionForHeading(`two`);
+dropDownFunctionForHeading(`three`);
+dropDownFunctionForHeading(`four`);
+dropDownFunctionForHeading(`five`);
